112424a1f7f44984a0ef7758fc772e28f429ff66
NMakarevich <nikoladze529@mail.ru>
2025-08-06 14:58:24 +0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: App,
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-page/login-page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'dashboard/:dashboardId',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard').then((m) => m.Dashboard),
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found-page/not-found-page').then(
+        (m) => m.NotFoundPage,
+      ),
+  },
+];
