217034be46703a2123296f802b7a6c9d8bc1dda1
NMakarevich <nikoladze529@mail.ru>
2025-08-06 22:16:30 +0300

    refactor: implement footer according task requirements

--- src/app/components/sidebar-footer/sidebar-footer.ts
+++ src/app/components/sidebar-footer/sidebar-footer.ts
@@ -1,16 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { AsyncPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-footer',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatButton, MatIcon],
   templateUrl: './sidebar-footer.html',
   standalone: true,
   styleUrl: './sidebar-footer.scss',
 })
 export class SidebarFooter {
   private readonly auth = inject(Auth);
+  private readonly router = inject(Router);
 
   profile$ = this.auth.profile;
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
 }
