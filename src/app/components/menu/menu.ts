00c43d3ced595f5dbd9591f33e9416abd33729ae
NMakarevich <nikoladze529@mail.ru>
2025-08-06 14:50:16 +0300

    refactor: add dashboard input

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,24 +1,16 @@
-import { Component } from '@angular/core';
-import { MenuModel } from './menu-model';
+import { Component, input } from '@angular/core';
 import { MatList, MatListItem } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
+import { DashboardsInterface } from '../../interfaces/smart-home-response';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatList, MatListItem, MatIcon],
+  imports: [MatList, MatListItem, MatIcon, RouterLink],
   templateUrl: './menu.html',
   standalone: true,
   styleUrl: './menu.scss',
 })
 export class Menu {
-  menuList: MenuModel[] = [
-    {
-      icon: 'dashboard',
-      title: 'Dashboard',
-    },
-    {
-      icon: 'info',
-      title: 'About',
-    },
-  ];
+  dashboards = input.required<DashboardsInterface[]>();
 }
