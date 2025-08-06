da3142baa0262ec986fd85f406dbbca9c6e2f680
NMakarevich <nikoladze529@mail.ru>
2025-08-06 14:47:00 +0300

    feat: add user info

--- src/app/components/sidebar-footer/sidebar-footer.ts
+++ src/app/components/sidebar-footer/sidebar-footer.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-footer',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './sidebar-footer.html',
   standalone: true,
   styleUrl: './sidebar-footer.scss',
 })
-export class SidebarFooter {}
+export class SidebarFooter {
+  private readonly auth = inject(Auth);
+
+  profile$ = this.auth.profile;
+}
