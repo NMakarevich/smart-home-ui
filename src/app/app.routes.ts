84b8faada4b769cc14ddfdde24e3531792262ab4
NMakarevich <nikoladze529@mail.ru>
2025-08-07 00:03:57 +0300

    fix: fix router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth-guard';
-import { App } from './app';
 
 export const routes: Routes = [
   {
     path: '',
-    component: App,
-  },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./pages/login-page/login-page').then((m) => m.LoginPage),
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
   {
     path: 'dashboard/:dashboardId',
     loadComponent: () =>
       import('./components/dashboard/dashboard').then((m) => m.Dashboard),
     canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-page/login-page').then((m) => m.LoginPage),
   },
   {
     path: '**',
