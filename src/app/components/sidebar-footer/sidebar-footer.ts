e3e9ca27adbb1df7cc9de643f1343066ebc5adc6
NMakarevich <nikoladze529@mail.ru>
2025-08-14 22:42:43 +0300

    feat: implement dashboard creation

--- src/app/components/sidebar-footer/sidebar-footer.ts
+++ src/app/components/sidebar-footer/sidebar-footer.ts
@@ -4,6 +4,8 @@ import { AsyncPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { DialogService } from '../../services/dialog.service';
+import { AddDashboardForm } from '../add-dashboard-form/add-dashboard-form';
 
 @Component({
   selector: 'app-sidebar-footer',
@@ -15,8 +17,13 @@ import { Router } from '@angular/router';
 export class SidebarFooter {
   private readonly auth = inject(Auth);
   private readonly router = inject(Router);
+  private readonly dialog = inject(DialogService);
 
   profile$ = this.auth.profile;
+
+  addDashboard() {
+    this.dialog.openDialog(AddDashboardForm);
+  }
 
   logout() {
     this.auth.logout();
