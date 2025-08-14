c4b99fe69689e45ee068ba4af760eff4f1b622ff
NMakarevich <nikoladze529@mail.ru>
2025-08-14 21:59:24 +0300

    feat: add dashboard service

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  DashboardInterface,
+  DashboardsInterface,
+} from '../interfaces/smart-home-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private readonly http = inject(HttpClient);
+
+  getDashboards() {
+    return this.http.get<DashboardsInterface[]>('/dashboards');
+  }
+
+  getDashboard(dashboardId: string) {
+    return this.http.get<DashboardInterface>(`/dashboards/${dashboardId}`);
+  }
+}
